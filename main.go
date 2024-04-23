d76dae1582c3787f938a5e58f3fefff0192dd3a8
Chris Amico <eyeseast@gmail.com>
2024-04-23 11:16:24 -0400

    maybe works

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 var helptext = "Usage: find-unused lang.json ./src"
@@ -19,7 +22,7 @@ func main() {
 	filename := args[0]
 	dir := args[1]
 
-	fmt.Println(filename, dir)
+	// fmt.Println(filename, dir)
 
 	lang, err := readJSON(filename)
 	if err != nil {
@@ -29,9 +32,20 @@ func main() {
 	queue := make(chan string)
 	go keys(lang, "", queue)
 
+	unused := make([]string, 0)
 	for key := range queue {
-		fmt.Println(key)
+		found, err := find(key, dir)
+		if (err) != nil {
+			log.Fatal(err)
+		}
+
+		if !found {
+			unused = append(unused, key)
+		}
 	}
+
+	fmt.Println("Unused keys:")
+	fmt.Println(strings.Join(unused, "\n"))
 }
 
 func readJSON(filename string) (map[string]interface{}, error) {
@@ -53,6 +67,7 @@ func keys(object map[string]interface{}, prior string, channel chan string) {
 		if prior != "" {
 			key = fmt.Sprintf("%s.%s", prior, k)
 		}
+
 		switch v.(type) {
 		case string:
 			channel <- key
@@ -66,4 +81,50 @@ func keys(object map[string]interface{}, prior string, channel chan string) {
 	if prior == "" {
 		close(channel)
 	}
+}
+
+// read a file and check if it contains a key
+func checkFile(filename string, key string) (bool, error) {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		return false, err
+	}
+
+	s := string(b)
+	return strings.Contains(s, key), nil
+}
+
+// walk a directory until we find a file containing key
+func find(key string, dir string) (bool, error) {
+	// fmt.Printf("Finding key: %s\n", key)
+	found := false
+	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		// go to next
+		if d.IsDir() {
+			return nil
+		}
+
+		if strings.HasPrefix(d.Name(), ".") {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		found, err = checkFile(path, key)
+		if err != nil {
+			return err
+		}
+
+		// if we found the key in a file, we're done
+		if found {
+			// log.Printf("Found %s in file %s", key, path)
+			return fs.SkipAll
+		}
+
+		return nil
+	})
+
+	return found, err
 }
